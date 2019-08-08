6b5287bd54983e487941bd36f7e0514e7c5aaa16
Weilin Gao <william.wlgao@gmail.com>
2019-08-08 18:30:29 +0800

    update the dfs solution for word search

---
+++ [79]Word_Search/sol_79.java
@@ -0,0 +1,41 @@
+public class sol_79 {
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0) {
+            return false;
+        }
+        if (word.length() == 0) {
+            return false;
+        }
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[i].length; i++){
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(board, word, i, j, 0)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean dfs(char[][] board, String word, int i, int j, int index) {
+
+        //the depth of dfs recursion is already the length of the target word, and the target word has been already found
+        // then stop the recursion
+        if (index == word.length()) {
+            return true;
+        }
+
+        //store the board elements temporarily
+        char temp = board[i][j];
+        board[i][j] = ' ';
+
+        // recursion
+        boolean isFound = dfs(board, word, i + 1, j, index + 1) ||
+                dfs(board, word, i - 1, j, index + 1) ||
+                dfs(board, word, i , j + 1, index + 1) ||
+                dfs(board, word, i , j - 1, index + 1);
+        board[i][j] = temp;
+        return isFound;
+    }
+}
