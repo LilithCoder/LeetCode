de3d57b4093dd56dc4cd6fa43b043c066586a7d6
Weilin Gao <william.wlgao@gmail.com>
2019-08-08 19:19:11 +0800

    remove some typo and missing statement, update the dfs solution for word search

--- [79]Word_Search/sol_79.java
+++ [79]Word_Search/sol_79.java
@@ -7,7 +7,7 @@ public class sol_79 {
             return false;
         }
         for (int i = 0; i < board.length; i++){
-            for (int j = 0; j < board[i].length; i++){
+            for (int j = 0; j < board[i].length; j++){
                 if (board[i][j] == word.charAt(0)) {
                     if (dfs(board, word, i, j, 0)){
                         return true;
