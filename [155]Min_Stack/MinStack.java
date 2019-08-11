f96682e56e91b8bcf4a8cd7aa7263734c2ccc11d
Weilin Gao <william.wlgao@gmail.com>
2019-08-11 20:08:18 +0800

    update the solution for [155] using two stacks

---
+++ [155]Min_Stack/MinStack.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+public class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty()) {
+            minStack.push(x);
+        } else {
+            minStack.push(Math.min(minStack.peek(), x));
+        }
+    }
+
+    public void pop() {
+        minStack.pop();
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
