78c529fda46fab5083c9fe0389719d5eafbc7cbe
Weilin Gao <weilinga@usc.edu>
2019-08-28 20:42:07 -0700

    update the solution for [136] whhich #1 uses hashest and #2 uses gitwise XOR hwhich is convenient and efficient

---
+++ [136]Single_Number/sol_136.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class sol_136 {
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num: nums) {
+            if (set.contains(num)) {
+                set.remove(num);
+            } else {
+                set.add(num);
+            }
+        }
+        for (int num: set) {
+            return num;
+        }
+        return -1;
+    }
+    /**
+     * use bitwise XOR to solve this problem
+     * bitwise XOR in java
+     *
+     * 0 ^ N = N
+     * N ^ N = 0
+     *
+     * 1 ^ 1 = 0
+     * 1 ^ 0 = 1
+     * if same, 0
+     * if not same, 1
+     */
+    public int singleNumber1(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+}
--- [819]Most_Common_Word/sol_819.java
+++ [819]Most_Common_Word/sol_819.java
@@ -1,2 +1,30 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class sol_819 {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        Set<String> bannedWords = new HashSet<>();
+        for (String bannedStr: banned) {
+            bannedWords.add(bannedStr);
+        }
+        Map<String, Integer> wordCounts = new HashMap<>();
+
+        // remove all non-letter character and transform to lowercase, then split them to individual words
+        for (String word: paragraph.replaceAll("[^a-zA-Z]", " ").toLowerCase().split(" ")) {
+            if (!bannedWords.contains(word)) {
+                wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+            }
+        }
+
+        // find the word with the largest count
+        String result = "";
+        for (String word: wordCounts.keySet()) {
+            if (result.equals("") ||  wordCounts.get(word) > wordCounts.get(result)) {
+                result = word;
+            }
+        }
+        return result;
+    }
 }
