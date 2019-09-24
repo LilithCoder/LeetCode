27a93913ad7e2c2d8dae03d61cd1fdd314e46dcc
Weilin Gao <weilinga@usc.edu>
2019-09-24 01:26:32 -0700

    update the [26] using twopointers

---
+++ [26]RemoveDuplicatesfromSortedArray/sol_26_twopointers.java
@@ -0,0 +1,11 @@
+public class sol_26_twopointers {
+    public int removeDuplicates(int[] nums) {
+        int left = 0;
+        for (int right = 1; right < nums.length; right++) {
+            if (nums[left] != nums[right]) {
+                nums[++left] = nums[right];
+            }
+        }
+        return left + 1;
+    }
+}
