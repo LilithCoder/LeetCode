64d6ceb94903affdddc1d3ba981c8bee11a11fb2
Weilin Gao <weilinga@usc.edu>
2019-09-27 02:00:06 -0700

    update solution for [112]using dfs

---
+++ [112]PathSum/sol_112_dfs.java
@@ -0,0 +1,18 @@
+public class sol_112_dfs {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+
+        if(root.left == null && root.right == null && sum - root.val == 0) return true;
+
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+
+    }
+}
