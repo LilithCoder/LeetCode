e554e1c72df30ece25de7dee025c28afd7c41740
Weilin Gao <weilinga@usc.edu>
2019-12-12 00:22:42 -0800

    update the solution for [160]

---
+++ [160]IntersectionOfTwoLinkedLists/sol_160.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class sol_160 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<ListNode>();
+        while (headA != null) {
+            visited.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            if (visited.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+}
