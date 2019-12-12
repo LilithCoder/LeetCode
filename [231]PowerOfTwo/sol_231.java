a0824130c5a3818f1c7d3daa6ff9a684b0cd50e7
Weilin Gao <weilinga@usc.edu>
2019-12-12 01:02:39 -0800

    update the solution for [231]

---
+++ [231]PowerOfTwo/sol_231.java
@@ -0,0 +1,9 @@
+package LeetCode;
+
+public class sol_231 {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) return false;
+        while (n % 2 == 0) n = n / 2;
+        return n == 1;
+    }
+}
