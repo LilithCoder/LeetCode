00a557e495a08b251152a71c91448fdde082315d
Weilin Gao <weilinga@usc.edu>
2019-12-18 20:34:48 -0800

    update the solution for [572] similar to lc same tree

---
+++ [572]SubtreeOfAnotherTree/sol_572.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class sol_572 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (isSameTree(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    public boolean isSameTree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        if (s.val != t.val) return false;
+        return isSameTree(s.right, t.right) || isSameTree(s.left, t.left);
+    }
+}
