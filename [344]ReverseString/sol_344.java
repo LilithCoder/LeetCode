22c52fdc20e452c635e7dd2011ec995bb98b0359
Weilin Gao <weilinga@usc.edu>
2019-12-28 02:06:54 -0800

    update the solution for [344]

---
+++ [344]ReverseString/sol_344.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+public class sol_344 {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+        while (start < end) {
+            char temp = s[end];
+            s[end--] = s[start];
+            s[start++] = temp;
+        }
+    }
+}
