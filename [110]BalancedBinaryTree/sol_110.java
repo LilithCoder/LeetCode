e1ec1e131ece9a6f28593bc6da5db91e7c0b5bd6
Weilin Gao <weilinga@usc.edu>
2019-12-28 22:10:17 -0800

    update the solution for [110]

---
+++ [110]BalancedBinaryTree/sol_110.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class sol_110 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return Math.abs(height(root.left) - height(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right);
+    }
+    public int height(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+}
