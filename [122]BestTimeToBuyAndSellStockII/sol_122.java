506b8116ffff947e1a6478c1207f62eec286b16c
Weilin Gao <weilinga@usc.edu>
2019-12-31 00:48:40 -0800

    update the solution for [122]

---
+++ [122]BestTimeToBuyAndSellStockII/sol_122.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+public class sol_122 {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return maxProfit;
+    }
+}
