8b34dabfeb91a53cb0ab873c1b5ac81d693ea0d5
Weilin Gao <weilinga@usc.edu>
2020-01-01 14:17:13 -0800

    update the solution for [31]

---
+++ [240]SearchA2DMatrixII/sol_240.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class sol_240 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int col = matrix[0].length - 1;
+        int row = 0;
+        while (row <= matrix.length - 1 && col >= 0) {
+            if (target == matrix[row][col]) {
+                return true;
+            } else if (target < matrix[row][col]) {
+                col--;
+            } else if (target > matrix[row][col]) {
+                row++;
+            }
+        }
+        return false;
+    }
+}
